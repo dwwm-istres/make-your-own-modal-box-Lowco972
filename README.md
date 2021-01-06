8a4377a536a73178c4bedadeb08168c3f7c2f4b1
Mathias - Afpa Istres <75681787+mathias-afpa@users.noreply.github.com>
2021-01-06 14:36:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# JS - EXO 5
+## Créer une boite modal
+
+Je vous ai préparé le squelette en HTML, vous devez réaliser les styles et le JavaScript qui va avec.
+
+* Vous devez faire disparaitre par défaut la div#modal
+* Lorsqu'on clique sur le bouton bleu la boite modal doit apparaitre
+* Une div avec un fond de couleur transparent doit couvrir l'ensemble de la page (à créer en JS).
+* Et lorsqu'on clique sur la petit croix dans la boite modal, elle doit se fermer, et la div colorée doit disparaitre.
+
+Un petit tuyau, la position absolute est nécessaire pour plusieurs éléments !
+
+À vous de jouer !
---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Put your JS here
+/*
+ * Don't forget to comment code
+*/
+
+// Set, in a variable, the button that controls the display of the modal box  (#modal-toggler)
+
+// Set, in a variable, the modal box (#modal).
+
+// Set, in a variable, the button that closes the modal box (#modal-toggler)
+
+// Create a new div element
+// Set the id for this new div element
+
+// Create a function named addOverlay which inserts a div, colored and lightly translucent in the page (#overlay).
+
+// Hook a lambda function to the click event on the button #modal-toggler.
+// Set a condition to the insertion of the colored div (#overlay), then show the modal box.
+
+// Hook a lambda function to the click event on the closing button #ferme.
