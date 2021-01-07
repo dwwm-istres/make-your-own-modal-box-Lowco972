9fa9d6b8e798618fce4d2aa34fced9aa8cdca295
Mathias - Afpa Istres <75681787+mathias-afpa@users.noreply.github.com>
2021-01-07 14:22:11 +0100

    Update app.js
    Réorganisation du fichier pour coller aux contraintes annoncées

--- js/app.js
+++ js/app.js
@@ -3,38 +3,29 @@
  * Don't forget to comment code
 */
 // je commence par faire mes variable
-const PopUp = document.getElementById('modalToggler');
-const ContenuModal = document.getElementById('modal');
-const ChangeColor = document.getElementById('NouvelleCouleur');
-const CroixModal = document.getElementById('close');
-//function
-document.getElementById('modalToggler').addEventListener("click", function () {
-    ContenuModal.style.display = "block";
-    ChangeColor.style.display = "block";
-})
-
-document.getElementById('close').addEventListener('click', function () {
-    ContenuModal.style.display = "none";
-    ChangeColor.style.display = "none";
-})
-
-
-
 
 
 
 
 // Set, in a variable, the button that controls the display of the modal box  (#modal-toggler)
-
+const PopUp = document.getElementById('modalToggler');
 // Set, in a variable, the modal box (#modal).
-
+const ContenuModal = document.getElementById('modal');
 // Set, in a variable, the button that closes the modal box (#modal-toggler)
-
+const CroixModal = document.getElementById('close');
 // Create a new div element
 // Set the id for this new div element
 // Create a function named addOverlay which inserts a div, colored and lightly translucent in the page (#overlay).
-
+const ChangeColor = document.getElementById('NouvelleCouleur');
+// function
 // Hook a lambda function to the click event on the button #modal-toggler.
 // Set a condition to the insertion of the colored div (#overlay), then show the modal box.
-
+document.getElementById('modalToggler').addEventListener("click", function () {
+    ContenuModal.style.display = "block";
+    ChangeColor.style.display = "block";
+})
 // Hook a lambda function to the click event on the closing button #ferme.
+document.getElementById('close').addEventListener('click', function () {
+    ContenuModal.style.display = "none";
+    ChangeColor.style.display = "none";
+})
