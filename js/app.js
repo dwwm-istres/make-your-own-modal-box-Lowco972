f4b6889ff67afbc94042352b6b42b84fb85069b5
Mathias <mathias.standaert@afpa.fr>
2021-01-07 14:14:42 +0100

    Fichiers fournis par jaguar network

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@
 /*
  * Don't forget to comment code
 */
+// je commence par faire mes variable
+const PopUp = document.getElementById('modalToggler');
+const ContenuModal = document.getElementById('modal');
+const ChangeColor = document.getElementById('NouvelleCouleur');
+const CroixModal = document.getElementById('close');
+//function
+document.getElementById('modalToggler').addEventListener("click", function () {
+    ContenuModal.style.display = "block";
+    ChangeColor.style.display = "block";
+})
+
+document.getElementById('close').addEventListener('click', function () {
+    ContenuModal.style.display = "none";
+    ChangeColor.style.display = "none";
+})
+
+
+
+
+
+
 
 // Set, in a variable, the button that controls the display of the modal box  (#modal-toggler)
 
@@ -11,7 +32,6 @@
 
 // Create a new div element
 // Set the id for this new div element
-
 // Create a function named addOverlay which inserts a div, colored and lightly translucent in the page (#overlay).
 
 // Hook a lambda function to the click event on the button #modal-toggler.
